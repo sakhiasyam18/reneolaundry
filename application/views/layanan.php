f99d10b14d8bd7c6999267d3c87d1f0d94bf1758
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2025-04-21 20:52:11 +0700

    in progess - layanan

--- application/views/layanan.php
+++ application/views/layanan.php
@@ -12,197 +12,211 @@
 </head>
 
 <body>
-    <!-- bagian satuan -->
-    <div>
-        <div class="col-md-6 mt-4 mt-md-0">
-            <div class="price-card">
-                <h5 class="mb-3">Bantal & Lainnya</h5>
-                <table class="satuan-table">
-                    <tr>
-                        <td>Bantal/guling besar</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                    <tr>
-                        <td>Bantal/guling sedang</td>
-                        <td>Rp 10.000</td>
-                    </tr>
-                    <tr>
-                        <td>Boneka kecil</td>
-                        <td>Rp 5.000</td>
-                    </tr>
-                    <tr>
-                        <td>Boneka sedang</td>
-                        <td>Rp 10.000</td>
-                    </tr>
-                    <tr>
-                        <td>Boneka besar</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                    <tr>
-                        <td>Tas kecil</td>
-                        <td>Rp 10.000</td>
-                    </tr>
-                    <tr>
-                        <td>Tas besar</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                    <tr>
-                        <td>Sepatu</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                </table>
+    <div class="container py-5">
+
+        <!-- bagian title -->
+        <div class="row mb-5">
+            <div class="col-12 text-center">
+                <h1>layanan <span>kami</span></h1>
+                <p class="text-secondary">Berbagai layanan laundry berkualitas dengan harga terjangkau</p>
             </div>
         </div>
 
-        <!-- pakaian dan kain -->
-        <div class="col-md-6">
-            <div class="price-card">
-                <h5 class="mb-3">Pakaian & Kain</h5>
-                <table class="satuan-table">
-                    <tr>
-                        <td>Selimut tipis</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                    <tr>
-                        <td>Selimut tebal</td>
-                        <td>Rp 22.000</td>
-                    </tr>
-                    <tr>
-                        <td>Bed cover</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                    <tr>
-                        <td>Gorden/m²</td>
-                        <td>Rp 4.000</td>
-                    </tr>
-                    <tr>
-                        <td>Karpet tipis/m²</td>
-                        <td>Rp 8.000</td>
-                    </tr>
-                    <tr>
-                        <td>Karpet tebal/m²</td>
-                        <td>Rp 10.000</td>
-                    </tr>
-                    <tr>
-                        <td>Bantal kecil</td>
-                        <td>Rp 5.000</td>
-                    </tr>
-                    <tr>
-                        <td>Tikar gulung</td>
-                        <td>Rp 15.000</td>
-                    </tr>
-                </table>
+        <!-- bagian satuan -->
+        <!-- layanan satuan title -->
+        <div class="row wb-5">
+            <div class="col-12 mb-4">
+                <h2 class="h3 fw-bold">Layanan Satuan</h2>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12 mb-4">
+                    <h2 class="h3">Bantal & Lainnya</h2>
+                    <table class="table table-hover">
+                        <tr>
+                            <td>Bantal/guling besar</td>
+                            <td class="text-end fw-bbold">Rp 15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Bantal/guling sedang</td>
+                            <td>Rp 10.000</td>
+                        </tr>
+                        <tr>
+                            <td>Boneka kecil</td>
+                            <td>Rp 5.000</td>
+                        </tr>
+                        <tr>
+                            <td>Boneka sedang</td>
+                            <td>Rp 10.000</td>
+                        </tr>
+                        <tr>
+                            <td>Boneka besar</td>
+                            <td>Rp 15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Tas kecil</td>
+                            <td>Rp 10.000</td>
+                        </tr>
+                        <tr>
+                            <td>Tas besar</td>
+                            <td>Rp 15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Sepatu</td>
+                            <td>Rp 15.000</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <!-- pakaian dan kain -->
+            <div class="col-md-6">
+                <div class="price-card">
+                    <h5 class="mb-3">Pakaian & Kain</h5>
+                    <table class="table table-hover">
+                        <tr>
+                            <td>Selimut tipis</td>
+                            <td>Rp 15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Selimut tebal</td>
+                            <td>Rp 22.000</td>
+                        </tr>
+                        <tr>
+                            <td>Bed cover</td>
+                            <td>Rp 15.000</td>
+                        </tr>
+                        <tr>
+                            <td>Gorden/m²</td>
+                            <td>Rp 4.000</td>
+                        </tr>
+                        <tr>
+                            <td>Karpet tipis/m²</td>
+                            <td>Rp 8.000</td>
+                        </tr>
+                        <tr>
+                            <td>Karpet tebal/m²</td>
+                            <td>Rp 10.000</td>
+                        </tr>
+                        <tr>
+                            <td>Bantal kecil</td>
+                            <td>Rp 5.000</td>
+                        </tr>
+                        <tr>
+                            <td>Tikar gulung</td>
+                            <td>Rp 15.000</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- bagian pricelist -->
+        <div>
+            <!-- ekononis 3 hari -->
+            <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header">3 Hari</div>
+                <div class="card-body">
+                    <h5 class="card-title">Ekonomis</h5>
+                    <ul>
+                        <li>
+                            <span>Cuci Setrika</span>
+                            <span>Rp 4.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Kering</span>
+                            <span>Rp 3.500,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Basah</span>
+                            <span>Rp 3.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Setrika</span>
+                            <span>Rp 3.000,-/kg</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- reguler 2 hari -->
+            <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header">3 Hari</div>
+                <div class="card-body">
+                    <h5 class="card-title">Ekonomis</h5>
+                    <ul>
+                        <li>
+                            <span>Cuci Setrika</span>
+                            <span>Rp 5.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Kering</span>
+                            <span>Rp 4.500,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Basah</span>
+                            <span>Rp 4.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Setrika</span>
+                            <span>Rp 4.000,-/kg</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- besok jadi 1 hari -->
+            <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header">1 Hari</div>
+                <div class="card-body">
+                    <h5 class="card-title">Besok jadi</h5>
+                    <ul>
+                        <li>
+                            <span>Cuci Setrika</span>
+                            <span>Rp 6.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Kering</span>
+                            <span>Rp 5.500,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Basah</span>
+                            <span>Rp 5.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Setrika</span>
+                            <span>Rp 5.000,-/kg</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- ekspress 6 jam -->
+            <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header">6 Jam</div>
+                <div class="card-body">
+                    <h5 class="card-title">Ekspres</h5>
+                    <ul>
+                        <li>
+                            <span>Cuci Setrika</span>
+                            <span>Rp8.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Kering</span>
+                            <span>Rp 7.500,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Cuci Basah</span>
+                            <span>Rp 7.000,-/kg</span>
+                        </li>
+                        <li>
+                            <span>Setrika</span>
+                            <span>Rp 7.000,-/kg</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-
-    <!-- bagian pricelist -->
-    <div>
-        <!-- ekononis 3 hari -->
-        <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">3 Hari</div>
-            <div class="card-body">
-                <h5 class="card-title">Ekonomis</h5>
-                <ul>
-                    <li>
-                        <span>Cuci Setrika</span>
-                        <span>Rp 4.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Kering</span>
-                        <span>Rp 3.500,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Basah</span>
-                        <span>Rp 3.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Setrika</span>
-                        <span>Rp 3.000,-/kg</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <!-- reguler 2 hari -->
-        <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">3 Hari</div>
-            <div class="card-body">
-                <h5 class="card-title">Ekonomis</h5>
-                <ul>
-                    <li>
-                        <span>Cuci Setrika</span>
-                        <span>Rp 5.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Kering</span>
-                        <span>Rp 4.500,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Basah</span>
-                        <span>Rp 4.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Setrika</span>
-                        <span>Rp 4.000,-/kg</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <!-- besok jadi 1 hari -->
-        <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">1 Hari</div>
-            <div class="card-body">
-                <h5 class="card-title">Besok jadi</h5>
-                <ul>
-                    <li>
-                        <span>Cuci Setrika</span>
-                        <span>Rp 6.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Kering</span>
-                        <span>Rp 5.500,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Basah</span>
-                        <span>Rp 5.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Setrika</span>
-                        <span>Rp 5.000,-/kg</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <!-- ekspress 6 jam -->
-        <div class="card text-bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">6 Jam</div>
-            <div class="card-body">
-                <h5 class="card-title">Ekspres</h5>
-                <ul>
-                    <li>
-                        <span>Cuci Setrika</span>
-                        <span>Rp8.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Kering</span>
-                        <span>Rp 7.500,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Cuci Basah</span>
-                        <span>Rp 7.000,-/kg</span>
-                    </li>
-                    <li>
-                        <span>Setrika</span>
-                        <span>Rp 7.000,-/kg</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
 </body>
 
 </html>
