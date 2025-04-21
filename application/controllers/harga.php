ff3624ae5f665c927c42418cf65c0562b01ef8e9
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2025-04-21 20:21:40 +0700

    bikin layanan harga harga

--- application/controllers/harga.php
+++ application/controllers/harga.php
@@ -1,15 +1,12 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class layanan extends CI_Controller
+class harga extends CI_Controller
 {
 
     public function index()
     {
         // Memuat view home.php
-        $this->load->view('layanan');
-
-        //helper url 
-        $this->load->helper('url');
+        $this->load->view('harga');
     }
 }
